ed4c44c828671fe44fe72018b57cd41f6e8e2d51
DrCopyPaste <DrCopyPaste@outlook.com>
2021-04-01 00:08:39 +0200

    wip logo as Svg (AboutWindow), use fork of SVGImage to allow runtime color changes

---
+++ SVGImage/SVG/FileLoaders/FileSystemLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace DotNetProjects.SVGImage.SVG.FileLoaders
+{
+    public class FileSystemLoader : IExternalFileLoader
+    {
+        static FileSystemLoader()
+        {
+            Instance = new FileSystemLoader();
+        }
+
+        public static FileSystemLoader Instance { get; }
+
+        public Stream LoadFile(string hRef, string svgFilename)
+        {
+            var path = Environment.CurrentDirectory;
+            if (!string.IsNullOrEmpty(svgFilename))
+            {
+                path = Path.GetDirectoryName(svgFilename);
+            }
+            string filename = Path.Combine(path, hRef);
+            if (File.Exists(filename))
+                return File.OpenRead(filename);
+
+            Trace.TraceWarning("Unresolved URI: " + hRef);
+
+            return null;
+        }
+    }
+}
