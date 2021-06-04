154dbc4a09af8d68f50823bac9ae16f2c871ce42
DrCopyPaste <DrCopyPaste@outlook.com>
2021-06-05 00:24:18 +0200

    create titlebar usercontrol

---
+++ RecNForget.Controls/TitleBar.xaml.cs
@@ -0,0 +1,82 @@
+﻿using RecNForget.IoC;
+using RecNForget.WPF.Services.Contracts;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Unity;
+
+namespace RecNForget.Controls
+{
+    /// <summary>
+    /// Interaction logic for TitleBar.xaml
+    /// </summary>
+    public partial class TitleBar : UserControl, INotifyPropertyChanged
+    {
+        private IActionService actionService = null;
+
+        private bool withSettingsButton = false;
+
+        public bool WithSettingsButton
+        {
+            get { return withSettingsButton; }
+            set { withSettingsButton = value; OnPropertyChanged(); }
+        }
+
+        private bool withMinimizeButton = false;
+
+        public bool WithMinimizeButton
+        {
+            get { return withMinimizeButton; }
+            set { withMinimizeButton = value; OnPropertyChanged(); }
+        }
+
+        private bool exitIsCancel = true;
+        public bool ExitIsCancel
+        {
+            get { return exitIsCancel; }
+            set { exitIsCancel = value; OnPropertyChanged(); }
+        }
+
+
+        public TitleBar()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                this.actionService = new RecNForget.Services.Designer.DesignerActionService();
+                return;
+            }
+            else
+            {
+                this.actionService = UnityHandler.UnityContainer.Resolve<IActionService>();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            actionService.ShowApplicationMenu();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var parentWindow = Window.GetWindow(this);
+            parentWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var parentWindow = Window.GetWindow(this);
+            parentWindow?.Close();
+        }
+    }
+}
